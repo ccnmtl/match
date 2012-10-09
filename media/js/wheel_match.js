bf9a96732d34c25a483e2c614ac5e0387fee460c
Zarina Mustapha <zarina@columbia.edu>
2012-10-09 15:21:09 -0400

    js and images for the wheel mouseover

---
+++ media/js/wheel_match.js
@@ -0,0 +1,36 @@
+	Image0 = new Image(384,667)
+	Image0.src = "/site_media/img/pediatricnutrition00.jpg"
+	Image1 = new Image(384,667)
+	Image1.src = "/site_media/img/pediatricnutrition01.jpg"
+	Image2 = new Image(384,667)
+	Image2.src = "/site_media/img/pediatricnutrition02.jpg"
+	Image3 = new Image(384,667)
+	Image3.src = "/site_media/img/pediatricnutrition03.jpg"
+	Image4 = new Image(384,667)
+	Image4.src = "/site_media/img/pediatricnutrition04.jpg"
+	Image5 = new Image(384,667)
+	Image5.src = "/site_media/img/pediatricnutrition05.jpg"
+	Image6 = new Image(384,667)
+	Image6.src = "/site_media/img/pediatricnutrition06.jpg"
+
+	function pediatricnutrition00() {
+	document.pediatricnutrition.src = Image0.src; return true; 
+	}
+	function pediatricnutrition01() {
+	document.pediatricnutrition.src = Image1.src; return true;
+	}
+	function pediatricnutrition02() {
+	document.pediatricnutrition.src = Image2.src; return true;
+	}
+	function pediatricnutrition03() {
+	document.pediatricnutrition.src = Image3.src; return true;
+	}
+	function pediatricnutrition04() {
+	document.pediatricnutrition.src = Image4.src; return true;
+	}
+	function pediatricnutrition05() {
+	document.pediatricnutrition.src = Image5.src; return true;
+	}
+	function pediatricnutrition06() {
+	document.pediatricnutrition.src = Image6.src; return true;
+	}
