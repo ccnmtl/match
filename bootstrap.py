1319e3b9c7398207816bbf19d85c2077956bda94
Anders Pearson <anders@columbia.edu>
2013-08-15 16:39:41 -0400

    wheelify

--- bootstrap.py
+++ bootstrap.py
@@ -5,51 +5,35 @@ import subprocess
 import shutil
 
 pwd = os.path.abspath(os.path.dirname(__file__))
-vedir = os.path.abspath(os.path.join(pwd, "ve"))
+vedir = os.path.abspath(os.path.join(pwd,"ve"))
 
-clear = True
-if len(sys.argv) > 1:
-    if sys.argv[1] == "--fast":
-        clear = False
-if clear and os.path.exists(vedir):
+if os.path.exists(vedir):
     shutil.rmtree(vedir)
 
-virtualenv_support_dir = os.path.abspath(
-    os.path.join(pwd, "requirements", "virtualenv_support"))
+virtualenv_support_dir = os.path.abspath(os.path.join(pwd, "requirements", "virtualenv_support"))
 
-ret = subprocess.call(["python", "virtualenv.py",
+ret = subprocess.call(["python", "virtualenv.py", 
                        "--extra-search-dir=%s" % virtualenv_support_dir,
                        "--never-download",
                        vedir])
+if ret: exit(ret)
+
+ret = subprocess.call(
+    [os.path.join(vedir, 'bin', 'pip'), "install",
+     "--index-url=http://pypi.ccnmtl.columbia.edu/",
+     "wheel==0.21.0"])
+
 if ret:
     exit(ret)
-
-if sys.version_info < (2, 7, 0):
-    ret = subprocess.call(
-        [os.path.join(vedir, 'bin', 'pip'), "install",
-         "-E", vedir,
-         os.path.join(pwd, "requirements/src/Imaging-1.1.7.tar.gz")])
-else:
-    ret = subprocess.call(
-        [os.path.join(vedir, 'bin', 'pip'), "install",
-         "-E", vedir,
-         os.path.join(pwd, "requirements/src/Pillow-1.7.8.zip")])
 
 ret = subprocess.call([os.path.join(vedir, 'bin', 'pip'), "install",
-                       "-E", vedir,
-                       "--enable-site-packages",
-                       "--index-url=''",
-                       "--requirement",
-                       os.path.join(pwd, "requirements/apps.txt")])
-if ret:
-    exit(ret)
+                       "--use-wheel",
+                       "--index-url=http://pypi.ccnmtl.columbia.edu/",
+                       "--requirement",os.path.join(pwd,"requirements.txt")])
+if ret: exit(ret)
 
-if sys.version_info < (2, 7, 0):
-    ret = subprocess.call(
-        [os.path.join(vedir, 'bin', 'pip'), "install",
-         "-E", vedir,
-         os.path.join(pwd, "requirements/src/importlib-1.0.1.tar.gz")])
-
-ret = subprocess.call(["python", "virtualenv.py", "--relocatable", vedir])
+ret = subprocess.call(["python","virtualenv.py","--relocatable",vedir])
 # --relocatable always complains about activate.csh, which we don't really
 # care about. but it means we need to ignore its error messages
+
+
