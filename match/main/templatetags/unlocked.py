1b270d09964e8af5e8f068cee63c9e1853d6ee4a
Susan Dreher <sdreher@columbia.edu>
2014-05-30 14:21:32 -0400

    ifsubmitted template tag for quizblock feedback

--- match/main/templatetags/unlocked.py
+++ match/main/templatetags/unlocked.py
@@ -16,7 +16,7 @@ class UnlockedNode(template.Node):
         u = r.user
         if _unlocked(u.get_profile(), s):
             return self.nodelist_true.render(context)
-        else:
+        elif self.nodelist_false:
             return self.nodelist_false.render(context)
 
 
@@ -31,3 +31,32 @@ def unlocked(parser, token):
     else:
         nodelist_false = None
     return UnlockedNode(section, nodelist_true, nodelist_false)
+
+
+class SubmittedNode(template.Node):
+    def __init__(self, section, nodelist_true, nodelist_false=None):
+        self.nodelist_true = nodelist_true
+        self.nodelist_false = nodelist_false
+        self.section = section
+
+    def render(self, context):
+        s = context[self.section]
+        r = context['request']
+        u = r.user
+        if s.submitted(u):
+            return self.nodelist_true.render(context)
+        elif self.nodelist_false:
+            return self.nodelist_false.render(context)
+
+
+@register.tag('ifsubmitted')
+def submitted(parser, token):
+    section = token.split_contents()[1:][0]
+    nodelist_true = parser.parse(('else', 'endifsubmitted'))
+    token = parser.next_token()
+    if token.contents == 'else':
+        nodelist_false = parser.parse(('endifsubmitted',))
+        parser.delete_first_token()
+    else:
+        nodelist_false = None
+    return SubmittedNode(section, nodelist_true, nodelist_false)
