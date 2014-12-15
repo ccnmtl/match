77c510d0c592331c996297812bb107de20c2d11e
Zarina Mustapha <zarina@columbia.edu>
2014-12-15 12:09:50 -0500

    changed reference to cunix static file locations

--- media/js/wheel_match.js
+++ media/js/wheel_match.js
@@ -1,17 +1,17 @@
 	Image0 = new Image(384,667)
-	Image0.src = "/site_media/img/pediatricnutrition00.jpg"
+	Image0.src = "https://www1.columbia.edu/sec/ccnmtl/remote/static/match/img/pediatricnutrition00.jpg"
 	Image1 = new Image(384,667)
-	Image1.src = "/site_media/img/pediatricnutrition01.jpg"
+	Image1.src = "https://www1.columbia.edu/sec/ccnmtl/remote/static/match/img/pediatricnutrition01.jpg"
 	Image2 = new Image(384,667)
-	Image2.src = "/site_media/img/pediatricnutrition02.jpg"
+	Image2.src = "https://www1.columbia.edu/sec/ccnmtl/remote/static/match/img/pediatricnutrition02.jpg"
 	Image3 = new Image(384,667)
-	Image3.src = "/site_media/img/pediatricnutrition03.jpg"
+	Image3.src = "https://www1.columbia.edu/sec/ccnmtl/remote/static/match/img/pediatricnutrition03.jpg"
 	Image4 = new Image(384,667)
-	Image4.src = "/site_media/img/pediatricnutrition04.jpg"
+	Image4.src = "https://www1.columbia.edu/sec/ccnmtl/remote/static/match/img/pediatricnutrition04.jpg"
 	Image5 = new Image(384,667)
-	Image5.src = "/site_media/img/pediatricnutrition05.jpg"
+	Image5.src = "https://www1.columbia.edu/sec/ccnmtl/remote/static/match/img/pediatricnutrition05.jpg"
 	Image6 = new Image(384,667)
-	Image6.src = "/site_media/img/pediatricnutrition06.jpg"
+	Image6.src = "https://www1.columbia.edu/sec/ccnmtl/remote/static/match/img/pediatricnutrition06.jpg"
 
 	function pediatricnutrition00() {
 	document.pediatricnutrition.src = Image0.src; return true; 
