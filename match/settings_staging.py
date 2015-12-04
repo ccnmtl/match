76020548d11ab5f52df29e49a811a5bad4d60b9e
Anders Pearson <anders@columbia.edu>
2015-12-04 12:16:35 +0000

    convert to ccnmtlsettings

--- match/settings_staging.py
+++ match/settings_staging.py
@@ -1,37 +1,15 @@
 # flake8: noqa
 from settings_shared import *
+from ccnmtlsettings.staging import common
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'match',
-        'HOST': '',
-        'PORT': 6432,
-        'USER': '',
-        'PASSWORD': '',
-    }
-}
-
-COMPRESS_ROOT = "/var/www/match/match/media/"
-
-TEMPLATE_DIRS = (
-    "/var/www/match/match/match/templates",
-)
-
-MEDIA_ROOT = '/var/www/match/uploads/'
-# put any static media here to override app served static media
-STATICMEDIA_MOUNTS = (
-    ('/sitemedia', '/var/www/match/match/sitemedia'),
-)
-
-DEBUG = False
-TEMPLATE_DEBUG = DEBUG
-SENTRY_SITE = 'match-staging'
-STATSD_PREFIX = 'match-staging'
-STAGING_ENV = True
-
-if 'migrate' not in sys.argv:
-    INSTALLED_APPS.append('raven.contrib.django.raven_compat')
+locals().update(
+    common(
+        project=project,
+        base=base,
+        STATIC_ROOT=STATIC_ROOT,
+        INSTALLED_APPS=INSTALLED_APPS,
+        s3static=False,
+    ))
 
 try:
     from local_settings import *
