e6dfb83ce86944015cf46d0c33ec4f9c48c1634c
Anders Pearson <anders@columbia.edu>
2016-06-07 13:54:20 +0100

    pagetree 0002_delete_testblock doesn't actually exist

--- match/main/migrations/0001_initial.py
+++ match/main/migrations/0001_initial.py
@@ -9,7 +9,7 @@ from django.conf import settings
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('pagetree', '0002_delete_testblock'),
+        ('pagetree', '0001_initial'),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
