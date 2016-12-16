8cb2000adfa6531c6685063be386ed0fa1e9adc1
Susan Dreher <sdreher@columbia.edu>
2016-12-16 13:39:41 -0500

    S3 Migration * add required libraries * update settings * remove templatetags from compressed blocks * setup a MEDIA_URL templatetag for custom pageblock template usage * make .css image references relative
    (Credentials are salted. S3 uploads bucket is populated.)

--- match/nutrition/urls.py
+++ match/nutrition/urls.py
@@ -1,11 +1,7 @@
-import django.views.static
-import os.path
 from django.conf.urls import include, url
 from match.nutrition.api import CounselingSessionResource, \
     CounselingSessionStateResource, DiscussionTopicResource
 from tastypie.api import Api
-
-media_root = os.path.join(os.path.dirname(__file__), "media")
 
 v1_api = Api(api_name='v1')
 v1_api.register(DiscussionTopicResource())
@@ -13,7 +9,5 @@ v1_api.register(CounselingSessionResource())
 v1_api.register(CounselingSessionStateResource())
 
 urlpatterns = [
-    url(r'^media/(?P<path>.*)$', django.views.static.serve, {
-        'document_root': media_root}),
     url(r'^api/', include(v1_api.urls)),
 ]
